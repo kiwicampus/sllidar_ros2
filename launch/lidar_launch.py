79ca3e4a6a934d33aeb8ea8a67f6ef891579fb54
garzonwilmer5 <garzonwilmer5@gmail.com>
2025-12-02 22:16:59 +0000

    [FEAT] update launch

--- launch/lidar_launch.py
+++ launch/lidar_launch.py
@@ -29,6 +29,11 @@ def generate_launch_description():
         s2_params if bool(int(os.getenv(key="LIDAR_TYPE", default=1))) else s1_params
     )
 
+    # Check if Nav2 stack is launched
+    # If Nav2 is running, the filter is loaded as a composable node in nav2_kronos container
+    # Otherwise, we launch it as a standalone node here
+    launch_nav2_stack = bool(int(os.getenv(key="LAUNCH_NAV2_STACK", default=0)))
+
     # Lidar 2D filter Params
     lidar2d_filter_params_file = None
     try:
@@ -38,27 +43,30 @@ def generate_launch_description():
             "lidar_filter_0deg.yaml",
         )
     except Exception as e:
-        print(f"Collision monitor failed reading params file. Got: {e}")
+        print(f"Lidar filter params file not found. Got: {e}")
 
-    return LaunchDescription(
-        [
-            Node(
-                package="sllidar_ros2",
-                executable="sllidar_node",
-                name="sllidar_node",
-                parameters=[lidar_params],
-                output="screen",
-                respawn=True,
-                respawn_delay=5.0,
-            ),
+    nodes = [
+        Node(
+            package="sllidar_ros2",
+            executable="sllidar_node",
+            name="sllidar_node",
+            parameters=[lidar_params],
+            output="screen",
+            respawn=True,
+            respawn_delay=5.0,
+        )
+    ]
+    if not launch_nav2_stack and lidar2d_filter_params_file:
+        nodes.append(
             Node(
                 executable="scan_to_scan_filter_chain",
                 package="laser_filters",
-                exec_name="scan_filter",
+                name="scan_to_scan_filter_chain",
                 parameters=[lidar2d_filter_params_file],
                 output="screen",
                 respawn=True,
                 respawn_delay=5.0,
-            ),
-        ]
-    )
+            )
+        )
+
+    return LaunchDescription(nodes)
